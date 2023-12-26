cad316f10627797c8415e8345c455b12d8435b44
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2023-12-26 22:13:23 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,25 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int inputNum = Integer.parseInt(args[0]);
+		String s = inputNum + " is a perfect number since " + inputNum + " = 1";
+		int sum = 1;
+		// checks for all inputNum divisors and sums them
+		for (int i = 2; i <= 0.5 * inputNum; i++)
+		{
+			if (inputNum % i == 0) 
+			{
+				sum += i;
+				s += " + " + i;
+			}
+		}
+		if (sum == inputNum)
+		{
+			System.out.println(s);
+		}
+		else{
+			System.out.println(inputNum + " is not a perfect number");
+		}
+		
 	}
 }
