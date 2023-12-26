73ad1041325271e5ee9ac2e2324334ea0581d620
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2023-12-26 19:33:50 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,21 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String inputS = args[0];
+		String newS = "";
+		char middleChar;
+		int lengthOfString = inputS.length();
+		if (lengthOfString % 2 == 1){
+			middleChar = inputS.charAt((lengthOfString - 1) / 2 );
+		} else {
+			middleChar = inputS.charAt(lengthOfString / 2 );
+		}
+		for (int n = lengthOfString - 1; n >= 0; n--)
+		{
+			newS += inputS.charAt(n);
+		}
+		System.out.println(newS);
+		System.out.println("The middle character is " + middleChar);
+			
 	}
 }
