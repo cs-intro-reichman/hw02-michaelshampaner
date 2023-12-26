f34cb83f99896cd0c5d5bd6998c89412a2b8cd35
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2023-12-26 17:57:59 +0200

    Update Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,17 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int input = Integer.parseInt(args[0]);
+		// checks if i is a divisor of input until reaches to value of input * 0.5
+		// no need to check further than 0.5 * input because its the largest integer possible to divide
+		for (int i = 1; i <= 0.5 * input; i++)
+		{
+			if (input % i == 0) 
+			{
+				System.out.println(i);
+			}
+		}
+		System.out.println(input);
+		
 	}
 }
