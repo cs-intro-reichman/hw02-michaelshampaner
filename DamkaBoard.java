d54b120f92b944b45c5bcb3d34233612129996b8
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2023-12-27 00:14:55 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,19 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		for (int i = 0; i < n; i++)
+		{
+			System.out.println("");
+			if (i % 2 == 1)
+			{
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++)
+			{
+				System.out.print("*");
+			}
+		}
+			
 	}
 }
