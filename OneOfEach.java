5541199001aa9e8ef68137f20837e30c8c5e3539
michaelshampaner <154332587+michaelshampaner@users.noreply.github.com>
2023-12-27 12:14:59 +0200

    Update OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -1,10 +1,36 @@
-
 /**
  *  Simulates the formation of a family in which the parents decide 
  *  to have children until they have at least one child of each gender.
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		boolean isBoy = false;
+		boolean isGirl = false;
+		int x = 0;
+		int count = 0;
+		while (isBoy == false || isGirl == false){
+			x = (int)(Math.random() * 2 );
+			count += 1;
+			if (x == 0){
+				isBoy = true;
+				if (count == 1){
+					System.out.print("b");
+				}
+				else{
+					System.out.print(" b");
+				}
+			}
+			else{
+				isGirl = true;
+				if (count == 1){
+					System.out.print("g");
+				}
+				else{
+					System.out.print(" g");
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("You made it... and you now have " + count + " children" );
 	}
 }
